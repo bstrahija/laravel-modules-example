fa68c216bf2933ee4d0b925b2fbf28e6110eb982
Boris Strahija <bstrahija@gmail.com>
2014-01-25 10:10:40 +0100

    Override config files

---
+++ app/config/packages/modules/content/channels.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+
+	'pages'    => array('title' => 'Pages'),
+	'blog'     => array('title' => 'Blog'),
+	'products' => array('title' => 'Products'),
+	'news'     => array('title' => 'News'),
+
+);
